ffaa5cb2207c9dd19120c1e55a6a83a792a9b9c7
Axel-Y <58230844+Axel-Y@users.noreply.github.com>
2023-02-06 20:33:26 -0600

    Update procesarReporte.php

--- reportes_CAASST/procesarReporte.php
+++ reportes_CAASST/procesarReporte.php
@@ -1,9 +1,22 @@
 <?php
-    $causa = $_POST['causa'];
-    $descripcion = $_POST['descripcion'];
-    $telefono = $_POST['telefonoUs'];
-    $nombre = $_POST['nombreUS'];
-    $ubicacion = $_POST['ubicacion'];
 
-    // Aquí puedes hacer algo con los datos recibidos, como insertarlos en una base de datos.
+if(isset($_POST["enviar"])) {
+  $causa = $_POST["causa"];
+  $descripcion = $_POST["descripcion"];
+  $telefono = $_POST["telefonoUs"];
+  $nombre = $_POST["nombreUS"];
+  $ubicacion = $_POST["ubicacion"];
+
+  // Validación de datos (opcional)
+  if(empty($causa) || empty($descripcion) || empty($telefono) || empty($nombre)) {
+    echo "Todos los campos son obligatorios";
+    exit;
+  }
+
+  // Procesamiento de datos (por ejemplo, guardar en una base de datos)
+
+  // Mensaje de éxito
+  echo "Reporte enviado exitosamente";
+}
+
 ?>
