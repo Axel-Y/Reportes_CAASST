a74869f341f4a7af9b0cf66a4c11547cdcd63e20
Axel-Y <58230844+Axel-Y@users.noreply.github.com>
2023-02-06 20:17:44 -0600

    Add files via upload

---
+++ reportes_CAASST/procesarReporte.php
@@ -0,0 +1,9 @@
+<?php
+    $causa = $_POST['causa'];
+    $descripcion = $_POST['descripcion'];
+    $telefono = $_POST['telefonoUs'];
+    $nombre = $_POST['nombreUS'];
+    $ubicacion = $_POST['ubicacion'];
+
+    // Aquí puedes hacer algo con los datos recibidos, como insertarlos en una base de datos.
+?>
